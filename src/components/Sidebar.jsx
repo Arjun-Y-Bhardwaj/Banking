17c51931b2437f8bb012b199e5bcf83f80fb0023
Arjun Bhardwaj <arjunyiannisbhardwaj@gmail.com>
2023-05-10 11:50:16 +0530

    header and sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,19 +9,19 @@ export default function Sidebar(props) {
     return (
         <div className='sidebar'>
             <div
-                className='sidebar-item'
+                className='sidebar-item s-i-bal'
             >
-                <div>Balance:&nbsp;</div> 
-                <div> ₹ {balance}</div>
+                <div s-i-bal-item>Balance:&nbsp;</div> 
+                <div s-i-bal-item> ₹ {balance}</div>
             </div>
             <div
-                className='sidebar-item'
+                className='sidebar-item s-i-d'
             >
                 <div>Total Deposit:&nbsp;</div>
                 <div>₹ {totalDeposit}</div>
             </div>
             <div
-                className='sidebar-item'
+                className='sidebar-item s-i-w'
             >
                 <div>Total Withdrawl:&nbsp;</div>
                 <div>₹ {totalWithdraw}</div>
