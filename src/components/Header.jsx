3cec485b0f350b8393c337dab784a7dcf4371109
Arjun Bhardwaj <arjunyiannisbhardwaj@gmail.com>
2023-05-10 12:08:10 +0530

    more

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import me from '../me.png'
 
 export default function Header() {
-    function handleMeClick() {
-
-    }
+   
     return (
         <div className='header-grid'>
             <div>
-                Welcome <span>ARJUN</span>
+                Welcome <span className='header-name'>ARJUN</span>
             </div>
             <div> <a href="https://www.linkedin.com/in/arjun-y-bhardwaj/"><img src={me} alt="" className='meimg' /></a> </div>
 
